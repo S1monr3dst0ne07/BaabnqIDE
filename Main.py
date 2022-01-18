ca13f9a1c98ef2d0ed9ba316b3d7421f0024c702
S1mon <jaforsi.kozak@gmail.com>
2022-01-18 18:04:12 +0100

    file saving working, indicator if file is saved

--- Main.py
+++ Main.py
@@ -7,8 +7,10 @@ import sys
 
 class cSender(QtCore.QObject):
     UpdateEditors = QtCore.pyqtSignal()
-
-
+    CloseTab4QWidget = QtCore.pyqtSignal(QtWidgets.QWidget)
+    RemoteDragEnterEvent = QtCore.pyqtSignal(QtGui.QDragEnterEvent)
+    RemoteDropEvent = QtCore.pyqtSignal(QtGui.QDropEvent)
+    UpdateTabSaveColor = QtCore.pyqtSignal()
 
 class cSyntaxHighlighter(QtGui.QSyntaxHighlighter):
     xStyles = {
@@ -97,9 +99,12 @@ class cCodeEditor(QtWidgets.QPlainTextEdit):
     def __init__(self, xSender):
         super().__init__()
         self.xFilePath = ""
+        self.xIsSaved = True
+        self.xRestoreIsSavedState = None
+        
         self.xSender = xSender
         xSender.UpdateEditors.connect(self.UpdateFromPath)
-        
+        self.textChanged.connect(self.Change)        
         
         self.InitUI()
     
@@ -111,8 +116,19 @@ class cCodeEditor(QtWidgets.QPlainTextEdit):
                 
         except FileNotFoundError:
             #if path was not found kill instance
-            self.close()
+            QtWidgets.QMessageBox.about(self, "File lost", f"Reference to file at {self.xFilePath} has been lost\nThis my be due to the deletion or renaming of that file".format())
+            self.xSender.CloseTab4QWidget.emit(self)
             
+    def Save(self):
+        try:
+            with open(self.xFilePath, "w") as xFileHandle:
+                xFileHandle.write(self.toPlainText())
+                
+            self.xIsSaved = True
+                
+        except Exception as E:
+            QtWidgets.QMessageBox.about(self, E)
+                
     
     def InitUI(self):
         self.setFont(QtGui.QFont("Consolas"))
@@ -121,14 +137,34 @@ class cCodeEditor(QtWidgets.QPlainTextEdit):
         
         self.verticalScrollBar().setStyleSheet(self.xScrollStyle.format(  sizeMod = "width:20px;", handelColor = "#444444"))
         self.horizontalScrollBar().setStyleSheet(self.xScrollStyle.format(sizeMod = "hight:20px;", handelColor = "#444444"))
+
+    def dragEnterEvent(self, xEvent):
+        self.xSender.RemoteDragEnterEvent.emit(xEvent)
+        self.xRestoreIsSavedState = self.xIsSaved
+        
+    def dropEvent(self, xEvent):
+        self.xSender.RemoteDropEvent.emit(xEvent)
+        self.xIsSaved = self.xRestoreIsSavedState
+        self.xSender.UpdateTabSaveColor.emit()
+        
+    def Change(self):
+        self.xIsSaved = False
+        self.xSender.UpdateTabSaveColor.emit()
         
         
 class cWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.xTabInstances = []
+        self.xTabContent = []
         self.xSender = cSender()
+        self.xSender.CloseTab4QWidget.connect(self.CloseTab4QWidget)
+        self.xSender.RemoteDragEnterEvent.connect(self.dragEnterEvent)
+        self.xSender.RemoteDropEvent.connect(self.dropEvent)
+        self.xSender.UpdateTabSaveColor.connect(self.UpdateTabSaveColor)
+        
+        self.setAcceptDrops(True)
+        
         self.InitUI()
 
     def InitUI(self):
@@ -140,9 +176,11 @@ class cWindow(QtWidgets.QMainWindow):
         QMenuBar {
             background-color: #333333;
             color: white;
-         
-         
         }               
+        QTabBar {background: gray;}
+        QTabBar::tab:selected {                            
+            background: #0000ff;                             
+        } 
         """)
 
         xCentralWidget = QtWidgets.QWidget(self)
@@ -151,54 +189,105 @@ class cWindow(QtWidgets.QMainWindow):
 
         
         self.xTabHost = QtWidgets.QTabWidget()
+        self.xTabHost.setStyleSheet("QWidget {background-color: #333333} QTabWidget::pane {border: 0;} QTabBar::tab {background: #333333;} ")
+        self.xTabHost.setTabsClosable(False)
         self.xMainLayout.addWidget(self.xTabHost)
 
         self.xMenu = self.menuBar()
         self.xMenuFile = self.xMenu.addMenu("&File")
         self.xMenuOptions = self.xMenu.addMenu("&Options")
 
-        self.xMenuFile.addAction(self.NewMenuSubOption("Open", self.OpenFileGui))
+        self.xMenuFile.addAction(self.NewMenuSubOption("Open File", self.OpenFileGui, "Ctrl+O"))
+        self.xMenuFile.addAction(self.NewMenuSubOption("Close File", lambda: self.CloseTab(self.xTabHost.currentIndex()), "Ctrl+W"))
+        self.xMenuFile.addAction(self.NewMenuSubOption("Save File", self.SaveFileGui, "Ctrl+S"))
+        self.xMenuFile.addAction(self.NewMenuSubOption("Refresh Editors", self.RefreshGui, ""))
+        self.xMenuFile.addAction(self.NewMenuSubOption("Exit", self.ExitGui, "Esc"))
+        
 
 
 
         self.show()
 
-
-    def NewMenuSubOption(self, xName, xActionFunc):
+    def NewMenuSubOption(self, xName, xActionFunc, xShort):
         xNewAction = QtWidgets.QAction(xName, self)
         xNewAction.triggered.connect(xActionFunc)
+        xNewAction.setShortcut(QtGui.QKeySequence(xShort))
         return xNewAction
 
-    
+    def RefreshGui(self):
+        xExitSaveDialog = QtWidgets.QMessageBox(self)
+        xExitSaveDialog.setWindowTitle("Confirm?")
+        xExitSaveDialog.setText("Refreshing will loose all done progress if not saved, do you wish to continue?")
+        xExitSaveDialog.setStandardButtons(QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
+        xExitSaveDialog.setDefaultButton(QtWidgets.QMessageBox.Cancel)
+        
+        xReturnCode = xExitSaveDialog.exec()
+        if xReturnCode == QtWidgets.QMessageBox.Ok: self.xSender.UpdateEditors.emit()
+        
+        
     def OpenFileGui(self):
-        xPath, _ = QtWidgets.QFileDialog.getOpenFileName(None, 'Open File', '', 'Baabnq File (*.baabnq)')
+        xPath, _ = QtWidgets.QFileDialog.getOpenFileName(None, 'Open File', '', 'Baabnq File (*.baabnq);;Text File (*.txt)')
         if xPath != "": 
             self.OpenCodeEditorTab(xPath)
 
+    def CloseTab4QWidget(self, QWidget):
+        self.CloseTab(self.xTabHost.indexOf(QWidget))
 
+    def CloseTab(self, xTabIndex):
+        if xTabIndex < 0: return
+        
+        self.xTabHost.removeTab(xTabIndex)
+        self.xTabContent[xTabIndex][0].close()
+        self.xTabContent.pop(xTabIndex)
+
+    def SaveFileGui(self):
+        self.xTabHost.currentWidget().Save()
+        self.xSender.UpdateTabSaveColor.emit()
+        
     def OpenCodeEditorTab(self, xPath): 
         xCodeEditor = cCodeEditor(self.xSender)
         xSyntaxHighlighter = cSyntaxHighlighter(xCodeEditor.document())
         
         self.xTabHost.addTab(xCodeEditor, self.Path2Name(xPath))
-        
-        #save reference to both the editor and it's highlighter
-        self.xTabInstances.append((xCodeEditor, xSyntaxHighlighter))
+        self.xTabContent.append((xCodeEditor, xSyntaxHighlighter))
         
         xCodeEditor.xFilePath = xPath
         self.xSender.UpdateEditors.emit()
         
+        xCodeEditor.xIsSaved = True
+        self.UpdateTabSaveColor()
+        
+    #tab changes color based on if saved or not
+    def UpdateTabSaveColor(self):
+        for xTabbarIndex in range(len(self.xTabHost)):
+            xCurrentWidget = self.xTabContent[xTabbarIndex][0]
+            if xCurrentWidget.xIsSaved: xColor = QtGui.QColor("#05A000")
+            else:                       xColor = QtGui.QColor("#FF5200")
+            self.xTabHost.tabBar().setTabTextColor(xTabbarIndex, xColor)
+
+    def ExitGui(self):
+        
+        self.close()
+        
     @staticmethod
     def Path2Name(xPath):
-        return re.search("\/([^\/]+)\.", xPath).group(1)
-
-    def keyPressEvent(self, xEvent):
-        if xEvent.key() == QtCore.Qt.Key_Escape:
-            self.close()
+        xMatch = re.search("[^\/]+\.[^\/]+$", xPath)
+        return xMatch[0]
 
 
+            
+    def dragEnterEvent(self, xEvent):
+        if xEvent.mimeData().hasText():
+            xEvent.accept()
+        else:
+            xEvent.ignore()
 
+    def dropEvent(self, xEvent):
+        xRawPath = xEvent.mimeData().text()
+        xPath = re.search("file:\/\/\/(.+)", xRawPath).group(1)
+        self.OpenCodeEditorTab(xPath)
 
+        
 
 if __name__ == '__main__':
     xApp = QtWidgets.QApplication(sys.argv)
